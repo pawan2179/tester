ccf57d3a3cd250dfda336d6c4a2ad4d3120de517
ace <pawansingh4418@gmail.com>
2025-04-27 18:36:16 +0530

    fixed candidate ui

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -8,6 +8,8 @@ import { api } from '../../../../convex/_generated/api';
 import { useQuery } from 'convex/react';
 import { useRouter } from 'next/navigation';
 import MeetingModal from '@/components/MeetingModal'
+import { Loader2Icon } from 'lucide-react';
+import MeetingCard from '@/components/MeetingCard';
 
 const Home = () => {
   const {isCandidate, isInterviewer, isLoading} = useUserRole();
@@ -70,7 +72,25 @@ const Home = () => {
       :
         <>
           <div>
-            candidate view
+            <h1 className='text-3xl font-bold'>Your Interviews</h1>
+            <p className='text-muted-foreground'>View and join your scheduled interviews</p>
+          </div>
+          <div className='mt-8'>
+            {interviews === undefined ? (
+              <div className='flex justify-center py-12'>
+                <Loader2Icon className='h-8 w-8 animate-spin text-muted-foreground' />
+              </div>
+            ): interviews.length > 0 ? (
+              <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+                {interviews.map((interview) => (
+                  <MeetingCard key={interview._id} interview={interview}/>
+                ))}
+              </div>
+            ) : (
+              <div className='text-center py-12 text-muted-foreground'>
+                You have no scheduled interviews
+              </div>
+            )}
           </div>
         </>
       }
